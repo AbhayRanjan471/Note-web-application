5358bdcbfdce4ca33b5bfd15e9ed5f2594688677
Abhay Ranjan <abhayranjan471@gmail.com>
2022-08-24 15:20:37 +0530

    added my notes we app

---
+++ index .js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port, function(err){
+    if(err){
+        // console.log('Error: ', err);
+             //OR
+        console.log(`Error in running the server: ${err}`)// using backtik ``
+    }
+    console.log(`Server is running on port: ${port}`);
+})
+
+
+
+
+
+
+/*
+step1: npm init
+step2: npm install express */
+
+/* to makke it the git repository to track all the changes
+step1: git init
+*/
